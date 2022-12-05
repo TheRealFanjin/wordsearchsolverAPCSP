1022ce08039a0178769524848dd36ad89419fc2f
fanjin <fanjin627@gmail.com>
2022-12-05 12:33:58 -0600

    changed __int__ to __init__

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow
 
 
 class MainWindow(QMainWindow):
-    def __int__(self):
+    def __init__(self):
         super().__init__()
         self.setWindowTitle("Word Search Solver")
 
